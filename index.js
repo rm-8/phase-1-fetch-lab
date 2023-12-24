6249f8f268c7d48f5339a3868661eb8381ec32b3
rm-8 <mohamedridhwan8@gmail.com>
2023-12-24 12:20:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+
+  fetch("https://anapioficeandfire.com/api/books")
+  .then(response => response.json())
+  .then(data => console.log(data))
 }
 
 function renderBooks(books) {
